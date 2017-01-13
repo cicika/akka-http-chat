36800743f76b8a9c18e0c47fa56e00541287e86f
Tijana Dojcinovic <cicika.t@gmail.com>
2017-01-13 20:23:30 +0100

    Add some commands for actors

--- src/main/scala/akka-http-chat/model/Commands.scala
+++ src/main/scala/akka-http-chat/model/Commands.scala
@@ -1,1 +1,12 @@
 package chat.model
+
+case class CreateUser(user: User)
+case class FetchUser(uuid: java.util.UUID)
+
+case class FetchMessages(conversation: Conversation)
+case class FetchUnreadMessages(conversation: Conversation, user: User)
+case class MarkMessagesRead(conversation: Conversation, user: User)
+case class MoveFromUnreadToMessages(conversation: Conversation)
+
+case class FetchConversations(user: Option[User])
+case class FetchConversation(conversation: Conversation)
