60f36cd8ef470efbc52a17201868dd00f38a85ec
Tijana Dojcinovic <cicika.t@gmail.com>
2017-01-14 15:34:54 +0100

    Start DatabaseActor on application start up

--- src/main/scala/akka-http-chat/Server.scala
+++ src/main/scala/akka-http-chat/Server.scala
@@ -2,13 +2,17 @@ package chat
 
 import akka.actor._
 import akka.http.scaladsl.Http
+import akka.routing._
 import akka.stream.ActorMaterializer
+
+import chat.db._
 
 object Server extends ChatService {
   def main(args: Array[String]) = {
     implicit val system = ActorSystem("akka-http-chat")
     implicit val materializer = ActorMaterializer()
     println("Starting up....")
+    system.actorOf(Props[DatabaseActor].withRouter(RoundRobinPool(10)), "database-actor")
     Http().bindAndHandle(chatRoute, "localhost", 8080)
   }
 }
