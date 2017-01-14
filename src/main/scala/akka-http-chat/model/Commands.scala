402fc59c09092c7b69ad3b2bfaf2784cef5249ba
Tijana Dojcinovic <cicika.t@gmail.com>
2017-01-14 17:13:50 +0100

    Remove unneeded case class. All messages will go straight to messages table, however if an open channel cannot be found for the recipient, it will be stored as UnreadMessage, until read

--- src/main/scala/akka-http-chat/model/Commands.scala
+++ src/main/scala/akka-http-chat/model/Commands.scala
@@ -6,7 +6,6 @@ case class FetchUser(uuid: java.util.UUID)
 case class FetchMessages(conversation: Conversation)
 case class FetchUnreadMessages(conversation: Conversation, user: User)
 case class MarkMessagesRead(conversation: Conversation, user: User)
-case class MoveFromUnreadToMessages(conversation: Conversation)
 
 case class FetchConversations(user: Option[User])
 case class FetchConversation(uuid: java.util.UUID)
