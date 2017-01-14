9d3d86759aa27a0da3be7235b0e9ee4c27630f44
Tijana Dojcinovic <cicika.t@gmail.com>
2017-01-14 16:05:14 +0100

    Add JSON support

---
+++ src/main/scala/akka-http-chat/http/ChatJsonProtocol.scala
@@ -0,0 +1,22 @@
+package chat.http
+
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import chat.model._
+import spray.json._
+
+trait ChatJsonProtocol extends SprayJsonSupport with DefaultJsonProtocol {
+
+  implicit object UuidJsonFormat extends RootJsonFormat[java.util.UUID] {
+    def write(uuid: java.util.UUID) = JsString(uuid.toString)
+
+    def read(value: JsValue) = value match {
+      case JsString(uuid) => java.util.UUID.fromString(uuid)
+      case x => deserializationError(s"Expected UUID as JsString, got $x")
+    }
+  }
+
+  implicit val userFormat = jsonFormat3(User)
+  implicit val messageFormat = jsonFormat4(Message)
+  implicit val unreadMessageFormat = jsonFormat5(UnreadMessage)
+  implicit val conversationFormat = jsonFormat2(Conversation)
+}
