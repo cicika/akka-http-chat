5fc81ddbbea4094c4f4d7c06e54b02362b0b7952
Tijana Dojcinovic <cicika.t@gmail.com>
2017-01-15 17:16:20 +0100

    Start conversation supervisor on app start up

--- src/main/scala/akka-http-chat/Server.scala
+++ src/main/scala/akka-http-chat/Server.scala
@@ -7,6 +7,7 @@ import akka.stream.ActorMaterializer
 
 import com.typesafe.config.ConfigFactory
 
+import chat.actor._
 import chat.db._
 
 object Server extends ChatService {
@@ -14,13 +15,13 @@ object Server extends ChatService {
     implicit val system = ActorSystem("akka-http-chat")
     implicit val materializer = ActorMaterializer()
     implicit val ec = system.dispatcher
-    
+
     println("Starting up....")
     val config = ConfigFactory.load()
     database.create()
 
     system.actorOf(Props[DatabaseActor].withRouter(RoundRobinPool(10)), "database-actor")
-    
+    system.actorOf(Props[ConversationSupervisorActor].withRouter(RoundRobinPool(20)), "conversation-supervisor")
     Http().bindAndHandle(chatRoute(system), "localhost", 8080)
   }
 }
